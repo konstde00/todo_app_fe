9f61d22bd5ac8f2277010eeef6d208d55784d003
kdementiev <kostiantyn.dementiev@observepoint.com>
2023-10-08 18:50:02 +0300

    Updated auth flow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import {ActivatedRoute, NavigationEnd, Router, RouterState} from "@angular/router";
+import {Title} from "@angular/platform-browser";
+import {DOCUMENT} from "@angular/common";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-app-fe';
+
+  constructor(
+    private router: Router,
+    private titleService: Title,
+    @Inject(DOCUMENT) private document: Document
+  ) {
+    this.handleRouteEvents();
+  }
+
+  handleRouteEvents() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        const title = this.getTitle(this.router.routerState, this.router.routerState.root).join('-');
+        this.titleService.setTitle(title);
+        gtag('event', 'page_view', {
+          page_title: title,
+          page_path: event.urlAfterRedirects,
+          page_location: this.document.location.href
+        })
+      }
+    });
+  }
+
+  getTitle(state: RouterState, parent: ActivatedRoute): string[] {
+    const data = [];
+    if (parent && parent.snapshot.data && parent.snapshot.data['title']) {
+      data.push(parent.snapshot.data['title']);
+    }
+    if (state && parent && parent.firstChild) {
+      data.push(...this.getTitle(state, parent.firstChild));
+    }
+    return data;
+  }
 }
