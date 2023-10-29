1fb7fe9dbd27acd7cea488bb3fe7e6eddf409589
kdementiev <kostiantyn.dementiev@observepoint.com>
2023-10-30 00:57:01 +0200

    Replaced localhost by value from env variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiHost: '',
-  googleAnalyticsApiKey: '',
+  apiHost: 'http://staging-todo-app-api-ext-9647-282134744.us-east-1.elb.amazonaws.com:80',
+  googleAnalyticsApiKey: 'G-THHWZZVRF3',
 };
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiHost: 'staging-todo-app-api-ext-9647-282134744.us-east-1.elb.amazonaws.com:80',
-  googleAnalyticsApiKey: '',
+  apiHost: 'http://staging-todo-app-api-ext-9647-282134744.us-east-1.elb.amazonaws.com:80',
+  googleAnalyticsApiKey: 'G-THHWZZVRF3',
 };
