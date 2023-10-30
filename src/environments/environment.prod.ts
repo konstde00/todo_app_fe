65d4ba6d67f2132e38b4e74b2be6a97d24a80b6c
kdementiev <kostiantyn.dementiev@observepoint.com>
2023-10-30 10:42:08 +0200

    Replaced localhost by value from env variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ import {IEnv} from "@app/src/environments/environment.helpers";
 
 export const environment: IEnv = {
   production: true,
-  apiHost: 'todo-app.konstde00.com',
+  apiHost: 'https://todo-app.konstde00.com',
   googleAnalyticsApiKey: 'G-THHWZZVRF3',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ import {IEnv} from "@app/src/environments/environment.helpers";
 
 export const environment: IEnv = {
   production: true,
-  apiHost: 'staging.todo-app.konstde00.com',
+  apiHost: 'https://staging.todo-app.konstde00.com',
   googleAnalyticsApiKey: 'G-J8H10NL7RB',
 };
